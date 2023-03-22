c312f0860c06249c7ac1d47472a9df030241244a
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-03-22 11:56:03 +0100

    refactor(1): rename and reorder

--- src/main/scala/exercise/Ex.scala
+++ src/main/scala/exercise/Ex.scala
@@ -11,10 +11,10 @@ object Ex extends App:
       case Cons(_, t) if n > 0 => drop(t, n - 1)
       case _ => l
 
-    def append[A](left: List[A], right: List[A]): List[A] = left match
-      case Nil() => right
-      case Cons(h, t) => Cons(h, append(t, right))
+    def append[A](l: List[A], r: List[A]): List[A] = l match
+      case Nil() => r
+      case Cons(h, t) => Cons(h, append(t, r))
 
     def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Nil() => Nil()
       case Cons(h, t) => append(f(h), flatMap(t)(f))
-      case _ => Nil()
