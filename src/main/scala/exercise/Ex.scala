fdcc7151c94a9005699e66fbb15c282c063209dd
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-03-22 11:57:02 +0100

    docs(1): add exercise number

--- src/main/scala/exercise/Ex.scala
+++ src/main/scala/exercise/Ex.scala
@@ -7,14 +7,17 @@ object Ex extends App:
     case Nil()
 
   object List:
+    // 1a
     def drop[A](l: List[A], n: Int): List[A] = l match
       case Cons(_, t) if n > 0 => drop(t, n - 1)
       case _ => l
 
+    // 1b
     def append[A](l: List[A], r: List[A]): List[A] = l match
       case Nil() => r
       case Cons(h, t) => Cons(h, append(t, r))
 
+    // 1c
     def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
       case Nil() => Nil()
       case Cons(h, t) => append(f(h), flatMap(t)(f))
